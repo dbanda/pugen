cb78db3ccb83e8757156fe3d533d6251f836a32f
Dalitso Banda <dbanda@us.ibm.com>
2015-09-27 22:08:05 -0400

    add log

--- foreva.sh
+++ foreva.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 echo "starting forever script"
 echo "...."
-forever -awl ~/logs/pugen.log --workingDir ~/Desktop/projects/pugen/ start ~/Desktop/projects/pugen/bin/www
-forever --workingDir ~/Desktop/projects/pugen/routes/stanford-nlp/stanford-postagger-full-2010-05-26/ -c bash start  ~/Desktop/projects/pugen/routes/stanford-nlp/stanford-postagger-full-2010-05-26/run-server.sh ~/Desktop/projects/pugen/routes/stanford-nlp/stanford-postagger-full-2010-05-26/models/left3words-wsj-0-18.tagger 9000
+forever --workingDir . -awl ./logs/pugen.log start -p . ./bin/www
+#forever --workingDir ./routes/stanford-nlp/stanford-postagger-full-2010-05-26/ -c bash start ./routes/stanford-nlp/stanford-postagger-full-2010-05-26/run-server.sh ./routes/stanford-nlp/stanford-postagger-full-2010-05-26/models/left3words-wsj-0-18.tagger 9000
 echo "foreva done"
 forever list
 
