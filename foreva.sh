7d04b19ca3f8210246cd9ef63c2ba98050229edb
Dalitso Banda <dbanda@us.ibm.com>
2015-11-04 02:24:17 -0500

    fixed startup scripts

--- foreva.sh
+++ foreva.sh
@@ -2,7 +2,7 @@
 echo "starting forever script"
 echo "...."
 echo "making log folder"
-mkdir ~/logs -p
+mkdir -p ~/logs
 echo "created log folder "
 forever --workingDir . -awl ~/logs/pugen.log -p . start ./bin/www
 forever --workingDir ./routes/stanford-nlp/stanford-postagger-full-2010-05-26/ -p . -awl ~/logs/stanford-nlp.log -c bash start ./routes/stanford-nlp/stanford-postagger-full-2010-05-26/run-server.sh ./models/left3words-wsj-0-18.tagger 9000
