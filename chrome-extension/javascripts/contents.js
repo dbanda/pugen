1bcdefed4b98ebff7e25e9b0fd3ba5263e322a0e
Dalitso Banda <dbanda@us.ibm.com>
2016-08-18 22:48:26 -0400

    run at end

--- chrome-extension/javascripts/contents.js
+++ chrome-extension/javascripts/contents.js
@@ -1,73 +1,76 @@
-    function inject(input) {
-        var username = $(input).wrap('<username></username>').parent()
-        username.css('position', 'relative')
-        $.get(chrome.extension.getURL('javascripts/pop.html'), function(data) {
-            var pop = $(data)
-            console.log(pop)
-                // pop.css('position', 'absolute')
-            var img = $(pop[0])
-            img.css('position', 'absolute')
-            var p = $(pop[1])
-            p.css('position', 'fixed')
-            img.attr('src', chrome.extension.getURL('javascripts/icon.png'))
-            p.hide()
-            console.log(img)
+function inject(input) {
+    
+    var username = $('<username></username>');
+    console.log(username);
+    $(input).parent().parent().prepend(username);
+    username.css('position', 'relative');
+    $.get(chrome.extension.getURL('javascripts/pop.html'), function(data) {
+        var pop = $(data)
+        console.log(pop)
+            // pop.css('position', 'absolute')
+        var img = $(pop[0])
+        img.css('position', 'absolute')
+        var p = $(pop[1])
+        p.css('position', 'fixed')
+        img.attr('src', chrome.extension.getURL('javascripts/icon.png'))
+        p.hide()
+        console.log(img)
 
-            p.click(function() {
-                p.toggle()
+        p.click(function() {
+            p.toggle()
 
-            })
-            img.click(function() {
-                var oldClick = $('body')[0].onclick
+        })
+        img.click(function() {
+            var oldClick = $('body')[0].onclick
 
-                $('body')[0].onclick = function() {
-                    console.log('clicked body')
-                    p.show();
-                    $('body')[0].onclick = function(){
-                        p.hide();
-                        $('body')[0].onclick = oldClick
-                    }
+            $('body')[0].onclick = function() {
+                console.log('clicked body')
+                p.show();
+                $('body')[0].onclick = function(){
+                    p.hide();
+                    $('body')[0].onclick = oldClick
                 }
+            }
 
 
-                p.show()
-                var password = p.find('#password')[0]
+            p.show()
+            var password = p.find('#password')[0]
 
-                var passwordText = customPassworsd()
-                p.find('#password').val(passwordText)
-                fill(passwordText)
-                window.getSelection().removeAllRanges();
-                // var password = document.querySelector('#password');
-                password.select();
-                var range = document.createRange();
-                range.selectNode(password);
-                window.getSelection().addRange(range);
+            var passwordText = customPassworsd()
+            p.find('#password').val(passwordText)
+            fill(passwordText)
+            window.getSelection().removeAllRanges();
+            // var password = document.querySelector('#password');
+            password.select();
+            var range = document.createRange();
+            range.selectNode(password);
+            window.getSelection().addRange(range);
 
-                try {
-                    // Now that we've selected the anchor text, execute the copy command  
-                    var successful = document.execCommand('copy');
-                    var msg = successful ? 'successful' : 'unsuccessful';
-                    console.log('Copy password command was ' + msg);
-                    p.find('#message').html('copied password to clipboard');
+            try {
+                // Now that we've selected the anchor text, execute the copy command  
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+                console.log('Copy password command was ' + msg);
+                p.find('#message').html('copied password to clipboard');
 
-                } catch (err) {
-                    console.log('Oops, unable to copy');
-                }
+            } catch (err) {
+                console.log('Oops, unable to copy');
+            }
+        })
+        var btn = p.find('button');
+        btn.hide();
+        btn.click(function() {
+                // body...
+                fill(customPassworsd)
             })
-            var btn = p.find('button');
-            btn.hide();
-            btn.click(function() {
-                    // body...
-                    fill(customPassworsd)
-                })
-                // pop.css('left', '110%')
-                // pop.css('top', '-10px')
-                // pop.css('width', '250px')
-            username.append(img)
-            $('body').prepend(p)
-        }, 'html')
+            // pop.css('left', '110%')
+            // pop.css('top', '-10px')
+            // pop.css('width', '250px')
+        username.append(img)
+        $('body').prepend(p)
+    }, 'html')
 
-    }
+}
 
     // chrome.runtime.onMessage.addListener(
     //   function(request, sender, sendResponse) {
@@ -76,61 +79,63 @@
     //     }
     // });
 
-    // inject($('input')[0])
-    var passwords = $('input[type=password]')
+// inject($('input')[0])
+// 
 
-    function fill(data) {
-        for (var i = passwords.length - 1; i >= 0; i--) {
-            $(passwords[i]).val(data)
-        }
+var passwords = $('input[type=password]')
+
+function fill(data) {
+    for (var i = passwords.length - 1; i >= 0; i--) {
+        $(passwords[i]).val(data)
     }
+}
 
-    if (passwords.length > 0) {
-        inject($(passwords[0]), passwords)
-            // fill(customPassworsd());
-    }
+if (passwords.length > 0) {
+    inject($(passwords[0]), passwords)
+        // fill(customPassworsd());
+}
 
-    // var generatePassword = require("password-generator");
-    var maxLength = 18;
-    var minLength = 12;
-    var uppercaseMinCount = 3;
-    var lowercaseMinCount = 3;
-    var numberMinCount = 2;
-    var specialMinCount = 2;
-    var pronouncable = true
+// var generatePassword = require("password-generator");
+var maxLength = 18;
+var minLength = 12;
+var uppercaseMinCount = 3;
+var lowercaseMinCount = 3;
+var numberMinCount = 2;
+var specialMinCount = 2;
+var pronouncable = true
 
-    var UPPERCASE_RE = /([A-Z])/g;
-    var LOWERCASE_RE = /([a-z])/g;
-    var NUMBER_RE = /([\d])/g;
-    var SPECIAL_CHAR_RE = /([\?\-])/g;
-    var NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
+var UPPERCASE_RE = /([A-Z])/g;
+var LOWERCASE_RE = /([a-z])/g;
+var NUMBER_RE = /([\d])/g;
+var SPECIAL_CHAR_RE = /([\?\-])/g;
+var NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
 
-    function isStrongEnough(password) {
-        var uc = password.match(UPPERCASE_RE);
-        var lc = password.match(LOWERCASE_RE);
-        var n = password.match(NUMBER_RE);
-        var sc = password.match(SPECIAL_CHAR_RE);
-        var nr = password.match(NON_REPEATING_CHAR_RE);
-        return password.length >= minLength &&
-            !nr &&
-            uc && uc.length >= uppercaseMinCount &&
-            lc && lc.length >= lowercaseMinCount &&
-            n && n.length >= numberMinCount &&
-            sc && sc.length >= specialMinCount;
-    }
+function isStrongEnough(password) {
+    var uc = password.match(UPPERCASE_RE);
+    var lc = password.match(LOWERCASE_RE);
+    var n = password.match(NUMBER_RE);
+    var sc = password.match(SPECIAL_CHAR_RE);
+    var nr = password.match(NON_REPEATING_CHAR_RE);
+    return password.length >= minLength &&
+        !nr &&
+        uc && uc.length >= uppercaseMinCount &&
+        lc && lc.length >= lowercaseMinCount &&
+        n && n.length >= numberMinCount &&
+        sc && sc.length >= specialMinCount;
+}
 
-    function customPassworsd() {
-        var password = "";
-        var randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
-        // while (!isStrongEnough(password)) {
-        password = generatePassword(randomLength, pronouncable, /[\w\d\?\-]/);
-        // s}
-        password += (Math.pow(10, numberMinCount) + Math.floor(Math.random() * (9 * Math.pow(10, numberMinCount))))
-        return password;
-    }
+function customPassworsd() {
+    var password = "";
+    var randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
+    // while (!isStrongEnough(password)) {
+    password = generatePassword(randomLength, pronouncable, /[\w\d\?\-]/);
+    // s}
+    password += (Math.pow(10, numberMinCount) + Math.floor(Math.random() * (9 * Math.pow(10, numberMinCount))))
+    return password;
+}
 
-    // console.log(customPassworsd())
-    // fill(customPassworsd())
-    console.log($('input'))
-        // $('input').val('input')
-    console.log(chrome.extension.getURL('pop.html'))
+// console.log(customPassworsd())
+// fill(customPassworsd())
+console.log($('input'))
+    // $('input').val('input')
+console.log(chrome.extension.getURL('pop.html'))
